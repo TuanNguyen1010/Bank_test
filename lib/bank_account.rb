756aebdcce359318493d1ffb21e4ef967fbd98a9
Tuan <Tuan.viet.nguyen@hotmail.co.uk>
2020-01-27 17:41:17 +0000

    transaction array added

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,12 +1,20 @@
-class Bank_account
-  attr_reader :balance
+require 'date'
+
+class BankAccount
+  attr_reader :balance, :transaction
 
   def initialize
     @balance = 0
+    @transaction = []
   end 
   
-  def deposit(amount)
+  def deposit(amount, date = DateTime.now.to_date.strftime("%d/%m/%Y"))
     @balance += amount
+    @transaction.push([date, ' || ', '%.2f' % amount, ' || || ', '%.2f' % @balance].join)
   end 
 
+  def withdraw(amount, date = DateTime.now.to_date.strftime("%d/%m/%Y"))
+    @balance -= amount
+    @transaction.push([date, ' || || ', '%.2f' % amount, ' || ', '%.2f' % @balance].join)
+  end 
 end 
--- spec/bank_account_spec.rb
+++ spec/bank_account_spec.rb
@@ -1,14 +1,28 @@
 require 'bank_account'
 
-describe Bank_account do 
+describe BankAccount do 
 
   before(:each) do 
-    @bank_account = Bank_account.new
+    @bank_account = BankAccount.new
   end 
 
   describe 'deposit' do
     it 'deposit 1000 into the account' do 
       expect { @bank_account.deposit(1000) } .to change { @bank_account.balance } .by(1000)
     end 
+  end 
+
+  describe 'withdraw' do 
+    it ' withdraw 500 from the account' do 
+      @bank_account.deposit(500)
+      expect { @bank_account.withdraw(500) }.to change { @bank_account.balance } .by(-500)
+    end 
+  end
+  
+  describe 'transaction' do 
+    it 'display amount,date and balance for deposit of 1000 on 10-01-2012' do
+      @bank_account.deposit(1000, "10/01/2012")
+      expect(@bank_account.transaction).to eq(['10/01/2012 || 1000.00 || || 1000.00'])
+    end  
   end 
 end 
